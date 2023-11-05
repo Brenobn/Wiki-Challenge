1625dbae8c6c3c5b6e086df7caaada92aa442e98
Brenobn <brenobragabn@gmail.com>
2023-11-05 11:34:01 -0300

    Mading a function using filter to remove repositories

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import gitLogo from '../assets/github.png';
 import Input from '../components/Input';
 import Button from '../components/Button';
-
+import ItemRepo from '../components/ItemRepo';
 import { api } from '../services/api';
 
 import { Container } from './styles';
@@ -28,15 +28,17 @@ const handleSearchRepo = async () => {
   alert('Repositório não encontrado')
 }
 
-
+const handleRemoveRepo = (id) => {
+  const updateRepos = repos.filter(repo => repo.id !== id);
+  setRepos(updateRepos);
+}
  
-
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="github logo" />
       <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value) } />
       <Button onClick={handleSearchRepo} />
-      
+      {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo={repo} />)}
     </Container>
   );
 }
