80d02f18ec727a337b1cfa0b1d9d1c445443e103
Brenobn <brenobragabn@gmail.com>
2023-11-05 11:29:08 -0300

    Creating function using find to search repositories

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import gitLogo from '../assets/github.png';
 import Input from '../components/Input';
 import Button from '../components/Button';
-import ItemRepo from '../components/ItemRepo';
+
 import { api } from '../services/api';
 
 import { Container } from './styles';
@@ -13,20 +13,30 @@ const [currentRepo, setCurrentRepo] = useState('');
 const [repos, setRepos] = useState([]);
 
 const handleSearchRepo = async () => {
-  const {data} = api.get(`repos/${currentRepo}`)
+  const {data} = await api.get(`repos/${currentRepo}`)
 
   if(data.id) {
-    setRepos(prev => [...prev, data]);
+    
+    const isExist = repos.find(repo => repo.id === data.id);
+
+    if(!isExist) {
+      setRepos(prev => [...prev, data]);
+      setCurrentRepo('');
+      return
+    }
   }
+  alert('Repositório não encontrado')
 }
+
+
  
 
   return (
     <Container>
       <img src={gitLogo} width={72} height={72} alt="github logo" />
-      <Input />
-      <Button />
-      <ItemRepo />
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value) } />
+      <Button onClick={handleSearchRepo} />
+      
     </Container>
   );
 }
